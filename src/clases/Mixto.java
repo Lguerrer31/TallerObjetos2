d4189f3a5e91d5c356c8dc62978bea070cc71aa6
Laura Guerrero <lauraguerrero3199@hotmail.com>
2016-09-08 00:05:10 -0500

    Avances

--- src/clases/Mixto.java
+++ src/clases/Mixto.java
@@ -10,5 +10,51 @@ package clases;
  * @author coste
  */
 public class Mixto {
+
+    private int entero;
+    private int numerador;
+    private int denominador;
+
+    public Mixto(int entero, int numerador, int denominador) throws DenominadorCeroException {
+        this.entero = entero;
+        this.numerador = numerador;
+        this.denominador = denominador;
+        if (denominador == 0) {
+            throw new DenominadorCeroException();
+        }
+    }
+
+    public int getEntero() {
+        return entero;
+    }
+
+    public void setEntero(int entero) {
+        this.entero = entero;
+    }
+
+    public int getNumerador() {
+        return numerador;
+    }
+
+    public void setNumerador(int numerador) {
+        this.numerador = numerador;
+    }
+
+    public int getDenominador() {
+        return denominador;
+    }
+
+    public void setDenominador(int denominador) {
+        this.denominador = denominador;
+    }
+    
+    public Mixto suma(Mixto m2, Mixto ent1){
+        int ent, num, den;
+        Mixto m;
+        num = ((this.entero * this.denominador) + this.numerador) + ((this.entero * m2.denominador) + m2.numerador);
+        den = this.denominador * m2.denominador;   
+        m = new Mixto(num, den);
+        return m;
+    }
     
 }
